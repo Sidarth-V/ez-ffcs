9c438d375560f15854ae32cc3cd04b0e1d4e0a1f
TJ202 <72973488+TJ202@users.noreply.github.com>
2022-06-17 23:44:13 +0530

    update: modal

--- src/components/feedback-form/feedback-form.component.jsx
+++ src/components/feedback-form/feedback-form.component.jsx
@@ -1,43 +1,41 @@
 import { fa1, fa2, fa3, fa4, fa5 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import RatingNumber from "../rating/rating.component";
 require("./feedback-form.styles.scss");
 
 const FeedbackForm = ({ rating, onSubmitHandler, feedback }) => {
   const [rate, setRate] = useState(rating);
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setRate(parseInt(e.target.value));
-  }
-  const num=[1,2,3,4,5];
+  };
+  const num = [1, 2, 3, 4, 5];
   const fas = [fa1, fa2, fa3, fa4, fa5];
   return (
     <form onSubmit={onSubmitHandler} className="fb-form">
       <fieldset>
         <legend>Feedback</legend>
         <textarea
-        rows={5}
-        placeholder="feedback goes here"
-        name="feedback"
-        defaultValue={feedback}
-      />
-      </fieldset>      
+          rows={5}
+          placeholder="feedback goes here"
+          name="feedback"
+          defaultValue={feedback}
+        />
+      </fieldset>
       <fieldset className="rating">
         <legend>Rating: </legend>
-        {num.map(i => (
-             <RatingNumber
-             newRating={rate}
-             val={i}
-             labelClass={"ra"+i}
-             faIcon={fas[i-1]}
-             faClass={"f fa"+i}
-             func={handleChange}
-           />
-        ))
-        }
+        {num.map((i) => (
+          <RatingNumber
+            newRating={rate}
+            val={i}
+            labelClass={`ra${i}`}
+            faIcon={fas[i - 1]}
+            faClass={`f fa${i}`}
+            func={handleChange}
+          />
+        ))}
       </fieldset>
       <div className="flexDiv">
-          <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" />
       </div>
     </form>
   );
